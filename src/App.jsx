13d3d6f26f449a0c3e3ccef6a15834d0dde2882c
PeaceNaza <peacedonatus38@gmail.com>
2024-04-21 00:25:56 +0100

    chore: Firebase project with react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./Components/Card"
+
+
+function App() {
+  return (
+    <>
+     
+      <h1 className="text-3xl font-bold underline text-center">Hello Ladies From Wtf24</h1>
+      <Card />
+    </>
+  )
+}
+
+export default App
