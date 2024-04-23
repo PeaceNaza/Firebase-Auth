be37b34c57a742c4d2160464d4532053e855a6db
PeaceNaza <peacedonatus38@gmail.com>
2024-04-23 13:46:04 +0100

    feat: Add footer component feat: Add signout functionality

--- src/Components/pages/Profile.jsx
+++ src/Components/pages/Profile.jsx
@@ -1,20 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom"
+import { auth } from "../../firebase"
+import { signOut } from "firebase/auth"
 
+function Profile() {
+  const navigate = useNavigate()
+  const user = auth.currentUser
 
-function Profile({user}) {
+  const signOutUser = async (e) => {
+    e.preventDefault()
+    
+    await signOut(auth)
+    navigate("/")
+  }
+
 
   return (
     <>
      <div className="flex justify-center mt-10">
       <div className="card w-96 bg-base-100 shadow-xl">
-      <h2 className="card-title">Welcome {user}</h2>
+      <h2 className="card-title">Welcome {user.email}</h2>
       
         <div className="card-body">
           
           <p>If a dog chews shoes whose shoes does he choose?</p>
 
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary" onClick={(e) => signOutUser(e)}>Logout</button>
           </div>
         </div>
       </div>
